ab7613088f96f2db12af84128f14b58bb882e0d1
Miguel Armendariz <miguel@dagm8.com>
2021-03-11 17:26:25 -0600

    headers that should be the same when tailwind is configured

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App = () => <div>React-app-template</div>;
+const App = () => (
+  <>
+    <h1>H1 header</h1>
+    <h2>H2 header</h2>
+    <h3>H3 header</h3>
+    <h4>H4 header</h4>
+  </>
+);
 
 export default App;
